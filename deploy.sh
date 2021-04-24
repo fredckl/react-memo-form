0666062b3274f593f7da1f61e413e49e75e323ea
Frédéric KOLLER <43694853+fredckl@users.noreply.github.com>
2021-04-24 08:55:25 +0200

    change directory

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,10 @@
 set -e
 
 # build
-npm run build
+yarn build
 
 # navigate into the build output directory
-cd dist
+cd build
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
